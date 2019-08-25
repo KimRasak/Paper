c2f561a8b14aa07519c60be56181642e5ce29180
KimRasak <657278923@qq.com>
2019-08-25 12:10:07 +0800

    ignore train.py

--- train5.py
+++ train5.py
@@ -1,10 +1,12 @@
 from Model.MDR_G6_att import MDR_G6_att
 from Model.utility.data_helper import Data
+import os
 
 if __name__ == '__main__':
-    path = "./data/30music"
+    path = "./data/aotm"
     num_epoch = 300
     batch_size = 512
 
-    data = Data(path, batch_size=batch_size, laplacian_mode="UPT", reductive_ut=True)
-    model = MDR_G6_att(num_epoch, data)
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    data = Data(path, pick=False, batch_size=batch_size, laplacian_mode="UPT", reductive_ut=True)
+    model = MDR_G6_att(num_epoch, data, embedding_size=64, learning_rate=1e-3, reg_rate=1e-3)
